fef52449ac554333ab54523811137b4eebfd131c
George Lungu <lungugeorge111@gmail.com>
2026-02-16 19:57:39 +0200

    my version of attack bullets movement

--- player/player.py
+++ player/player.py
@@ -5,11 +5,10 @@ class Player:
 
     keyboard_handler = None
     color = "white"
-    player_pos = None
+    pos = None
     screen = None
     keyPressed = None
     direction = None
-    do_it = True
     attack_speed = None
     last_moving_direction = None
 
@@ -17,54 +16,40 @@ class Player:
         self.screen = screen
         self.keyboard_handler = keyboard_handler
         self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        self.pos = pygame.Vector2(self.player_pos.x, self.player_pos.y)
+
+    def _move_vector_x_y(self, vector_pos, direction, speed):
+        if direction == MovingDirection.UP:
+            self.color = "black"
+            vector_pos.y -= speed
+        elif direction == MovingDirection.DOWN:
+            vector_pos.y += speed
+            self.color = "blue"
+        elif direction == MovingDirection.LEFT:
+            vector_pos.x -= speed
+            self.color = "grey"
+        elif direction == MovingDirection.RIGHT:
+            vector_pos.x += speed
+            self.color = "yellow"
+        return vector_pos
 
     def update_player(self, dt):
         self.direction = self.keyboard_handler.get_movement_direction()
         self.last_moving_direction = self.keyboard_handler.get_last_movement_direction()
-
-        if self.direction == MovingDirection.UP:
-            self.color = "black"
-            self.player_pos.y -= 300 * dt
-        elif self.direction == MovingDirection.DOWN:
-            self.player_pos.y += 300 * dt
-            self.color = "blue"
-        elif self.direction == MovingDirection.LEFT:
-            self.player_pos.x -= 300 * dt
-            self.color = "grey"
-        elif self.direction == MovingDirection.RIGHT:
-            self.player_pos.x += 300 * dt
-            self.color = "yellow"
-
-        self.attack_speed = 1000 * dt
+        self.player_pos = self._move_vector_x_y(self.player_pos, self.direction, 300 * dt)
+        self.attack_speed = 3000 * dt
         if self.keyboard_handler.space_pressed:
-            self.draw_attack(self.player_pos)
+            self.draw_attack()
 
     def draw_player(self):
         pygame.draw.circle(self.screen, self.color, self.player_pos, 40)
 
-
-    def draw_attack(self, player_pos):
-        duration_counter = 100000
-        if self.do_it:
-            x = player_pos.x
-            y = player_pos.y
-            pos = pygame.Vector2(x, y)
-
-            while duration_counter > 0:
-                duration_counter -= 1
-
-
-                if self.last_moving_direction == MovingDirection.UP:
-                    pos.y -= self.attack_speed
-                elif self.last_moving_direction == MovingDirection.DOWN:
-                    pos.y += self.attack_speed
-                elif self.last_moving_direction == MovingDirection.LEFT:
-                    pos.x -= self.attack_speed
-                elif self.last_moving_direction == MovingDirection.RIGHT:
-                    pos.x += self.attack_speed
-
-
-                pygame.draw.circle(self.screen, "red", pos , 40)
-                self.do_it = False
-                if duration_counter == 0:
-                    self.do_it = True
+    def draw_attack(self):
+        max_distance = 10
+        while max_distance > 0:
+            max_distance -= 1
+            self.pos = self._move_vector_x_y(self.pos, self.last_moving_direction, self.attack_speed)
+            pygame.draw.circle(self.screen, "red", self.pos, 40)
+        else:
+            self.pos.x = self.player_pos.x
+            self.pos.y = self.player_pos.y
